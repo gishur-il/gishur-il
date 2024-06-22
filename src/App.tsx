2ff62680f93a6ad5de320d1bf29b7713ebae1518
itamar.bar-lev <itamar.bar-lev@appsflyer.com>
2024-06-22 14:41:28 +0300

    Bini

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./Header";
+import Bini from "./Bini";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Bini />
     </div>
   );
 };
--- src/Header.tsx
+++ src/Header.tsx
@@ -5,14 +5,16 @@ import logo from "./gishur-logo-brighter.png";
 export default function () {
   return (
     <header>
-      <div className="horizontal">
-        <img src={logo} alt="logo" />
-        <h1>גישור ישראל</h1>
-      </div>
+      <div className="frame">
+        <div className="horizontal">
+          <img src={logo} alt="גישור ישראל" />
+          <h1>גישור ישראל</h1>
+        </div>
 
-      <div className="content">
-        <h2>נכנסים במחלוקות מסיימים בהבנות</h2>
-        <button>צרו קשר</button>
+        <div className="content">
+          <h2>נכנסים במחלוקות מסיימים בהבנות</h2>
+          <button>צרו קשר</button>
+        </div>
       </div>
     </header>
   );
