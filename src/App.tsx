aaf84c1992cb22af4d1f30d6120cf0c93bb97ea1
itamar.bar-lev <itamar.bar-lev@appsflyer.com>
2024-06-22 15:49:24 +0300

    Family

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./Header";
 import Bini from "./Bini";
+import Family from "./Family";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Bini />
+      <Family />
     </div>
   );
 };
